075da05f2e280209f40a2c9aca72d1a5ed685657
Cassie <cassie.pericak@northwestern.edu>
2017-03-13 13:55:49 -0500

    create skeleton and connected to db

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Initialize Express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Require external files
+require("./routes/routes.js");
+
+//Require db connection
+var db = require("./controller/connection.js");
+
+// Set the app to listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
+});
